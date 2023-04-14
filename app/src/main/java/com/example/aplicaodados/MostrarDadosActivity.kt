827e1c988c5c8b0883e8491cd02b36ab2b5d0f6b
Alexandre Rossa <alexxandrerossa@gmail.com>
2023-04-14 09:52:36 +0100

    Criação MostrarDadosActivity.kt ✔️

---
+++ app/src/main/java/com/example/aplicaodados/MostrarDadosActivity.kt
@@ -0,0 +1,17 @@
+package com.example.aplicaodados
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class MostrarDadosActivity : AppCompatActivity()  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mostradados)
+
+
+    }
+}
